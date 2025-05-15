742e4f8250629dab9a90d3010ba866857dfcf65e
Aryan <1706.aryanjain@gmaill.com>
2025-05-15 15:18:46 +0530

    Final commit for this project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-import React from "react";
-import Heading from "./Header";
+import React, { useState } from "react";
+import Header from "./Header";
 import Footer from "./Footer";
-import Notes from "./Notes";
-import notes from "../notes";
+import Note from "./Notes";
+import CreateArea from "./Create_area";
 
-function App(){
-    return <div>
-        <Heading/>
-        {notes.map(noteItem => <div>
-            <Notes title={noteItem.title} content={noteItem.content} key={noteItem.key} />
-        </div>)}
-        <Footer/>
-    </div>;
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
+
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import WbIncandescentIcon from '@mui/icons-material/WbIncandescent';
+
+
 
 function Heading(){
-    return <header><h1>The Remembrall</h1></header>;
+    return <header><h1><WbIncandescentIcon />The Remembrall</h1></header>;
 }
 export default Heading;
